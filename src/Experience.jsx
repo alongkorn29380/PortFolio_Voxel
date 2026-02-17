e43f824f90eef7456dca4bbe929a88353cec3ebf
alongkorn anuwatprakit <alongkorn29380@gmail.com>
2026-02-17 16:05:36 +0700

    camera

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,11 +1,17 @@
+import { OrbitControls } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
 import { Suspense } from 'react'
+
 import Terrain from './components/Terrain/Terrain'
 
 export default function Experience(){
     return(
         <>
+            // Efficiency
             <Perf position="top-left" />
+
+            // Camera controls
+            <OrbitControls makeDefault />
 
             <ambientLight intensity={1.5} /> 
             
