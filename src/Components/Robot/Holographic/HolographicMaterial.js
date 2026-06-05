47a7cd2defe9b8889274aaa3a45549ba80196080
Alongkorn Anuwatprakit <alongkorn29380@gmail.com>
2026-06-05 13:57:27 +0700

    feat: extract HolographicMaterial into shared module

---
+++ src/Components/Robot/Holographic/HolographicMaterial.js
@@ -0,0 +1,16 @@
+import { extend } from '@react-three/fiber'
+import { shaderMaterial } from '@react-three/drei'
+import * as THREE from 'three'
+
+import holographicVertexShader from './shaders/holographic/vertex.glsl'
+import holographicFragmentShader from './shaders/holographic/fragment.glsl'
+
+const HolographicMaterial = shaderMaterial(
+    { uTime: 0, uColor: new THREE.Color('#70c1ff') },
+    holographicVertexShader,
+    holographicFragmentShader
+)
+
+extend({ HolographicMaterial })
+
+export { HolographicMaterial }
