874afac44229d7ae8f2f9c921c9925fc7895dc8f
Alongkorn Anuwatprakit <alongkorn29380@gmail.com>
2026-06-05 14:01:55 +0700

    feat: add HolographicSatellite and HolographicCosmic to Robot scene

--- src/Components/Robot/Robot.jsx
+++ src/Components/Robot/Robot.jsx
@@ -1,6 +1,9 @@
 import RobotBushes from './RobotBushes.jsx'
 import RobotTree from './RobotTrees.jsx'
 import Sensor from './Sensor.jsx'
+import Holographic from './Holographic/Holographic.jsx'
+import HolographicSatellite from './Holographic/HolographicSatellite.jsx'
+import HolographicCosmic from './Holographic/HolographicCosmic.jsx'
 
 export default function Robot({ nodes })
 {
@@ -9,6 +12,9 @@ export default function Robot({ nodes })
             <RobotBushes nodes={ nodes } />
             <RobotTree nodes={ nodes } />
             <Sensor nodes={ nodes } />
+            <Holographic />
+            <HolographicSatellite />
+            <HolographicCosmic />
         </>
     )
 }
